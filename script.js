c3c81f122bcb86a357ce40fb27538a7e28f9642a
Himanshu <himanshux412@gmail.com>
2026-02-26 12:11:25 +0530

    new

--- script.js
+++ script.js
@@ -20,7 +20,35 @@ let name ="Hashi";
 console.log(name); */
 
 //const  variable
-{
+/* {
 const name ="Himanshu";
 console.log(name); }
-console.log(name);
+console.log(name); */
+ 
+//opertators
+// arithmetic operators
+/* let a = 10;
+let b=5;
+console.log(a+b); //15
+console.log(a-b); //5
+console.log(a*b); //50
+console.log(a/b); //2
+console.log(a%b); //0
+console.log(a++); //10
+console.log(++a); //12
+console.log(--a); //11
+console.log(a--); //11
+console.log(a); //10 */
+ 
+//Assignment operators
+let a=10;
+a+=5; // a=a+5
+console.log(a); //15
+a-=3; // a=a-3
+console.log(a); //12
+a*=2; // a=a*2
+console.log(a); //24
+a/=4; // a=a/4
+console.log(a); //6
+a%=3; // a=a%3
+console.log(a); //0 
