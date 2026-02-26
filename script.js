25357182f94452f1e505dc448dc50838e6a50467
Himanshu <himanshux412@gmail.com>
2026-02-26 11:49:28 +0530

    new

---
+++ script.js
@@ -0,0 +1,26 @@
+ // console.log("Hello, World!");
+
+// single line comment
+
+/* multi-line comment */
+
+//Inntroduction to Varivale:var;
+/* {var a=20;
+a="Himanshu";
+console.log(a); }
+console.log(a);
+
+//variable :const */
+/* {let name ;
+name ="Himanshu"
+name = "Himanshu Singh";
+console.log(name);
+}
+let name ="Hashi";
+console.log(name); */
+
+//const  variable
+{
+const name ="Himanshu";
+console.log(name); }
+console.log(name);
