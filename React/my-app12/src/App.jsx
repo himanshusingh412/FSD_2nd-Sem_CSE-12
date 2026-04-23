50a604f0e2075c0307ad734037aadf9069b739fc
Himanshu <himanshux412@gmail.com>
2026-04-23 12:19:23 +0530

    new

---
+++ React/my-app12/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+      <p>This is a simple React application.</p>
+      <CSE12 />
+    </div>
+  );
+}
+
+//2nd component
+function CSE12() {
+  return (
+    <div className="CSE12">
+      <h2>Welcome to CSE12</h2>
+      <p>This is the CSE12 component.</p>
+    </div>
+  );
+}
+export default App;
